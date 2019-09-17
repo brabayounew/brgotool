a8f102fc8d6993b97994cd0352f2bdfe7e412d27
yangwenbin <brabais1dba@163.com>
2019-09-17 15:47:26 +0800

    定义了一个工具方法

---
+++ src/top/bigz/brgo/tools/ArrayPu.java
@@ -0,0 +1,27 @@
+package top.bigz.brgo.tools;
+
+/**
+ * 包含Array 相关的各种工具方法
+ *
+ * @author yangwenbin
+ */
+public class ArrayPu {
+
+    /**
+     * 将 inArray 按照 size 分隔成固定长度的 数组
+     * 并将它们组成一个新的数组
+     * 如果最后剩余部分不足以组成一个size 长度 那么它们单独组成一个区块
+     *
+     * chunk(['a', 'b', 'c', 'd'], 3) => [['a', 'b', 'c'], ['d']]
+     *
+     *
+     * @param inArray 待处理的数组
+     * @param size 区块长度
+     * @return 组成的新区块
+     *
+     * @author yangwenbin
+     */
+    public Object[] chunk(Object[] inArray, int size) {
+        return null;
+    }
+}
